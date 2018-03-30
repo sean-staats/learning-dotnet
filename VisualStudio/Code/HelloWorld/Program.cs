61816961e107856ebc740320ad8f57f3f5d0cc25
Sean Staats <seans@genebygene.com>
2018-03-30 17:49:37 -0500

    Initial commit

---
+++ VisualStudio/Code/HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is your name? ");
+            string name = Console.ReadLine();
+            DateTime dt = DateTime.Now;
+            Console.WriteLine($"Hello, {name}!  The current time is {dt}");
+            Console.Write("Press any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ VisualStudio/Professional/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("\nWhat is your name? ");
+            string name = Console.ReadLine();
+            DateTime date = DateTime.Now;
+            Console.WriteLine($"\nHello, {name}, on {date:d} at {date:t}!");
+            Console.Write("\nPress any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
